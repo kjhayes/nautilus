2a407bbdffb330f65bd3f76fcc874f33d2e5a846
Nick Wanninger <nickwanninger@gmail.com>
2024-04-15 15:51:20 -0500

    update default.nix to build isoimage

--- default.nix
+++ default.nix
@@ -16,10 +16,9 @@ pkgs.stdenv.mkDerivation {
     grub2
 
     # Compiler dependencies
-    llvmPackages_16.libllvm
-    llvmPackages_16.clang
-    llvmPackages_16.stdenv
+    gcc
     binutils
+    xorriso
     gllvm
 
     bashInteractive
@@ -36,8 +35,7 @@ pkgs.stdenv.mkDerivation {
 
   buildPhase = ''
     make V=1 -j $NIX_BUILD_CORES
-    # TODO: grub-mkrescue isn't working right now...
-    # make -j $NIX_BUILD_CORES isoimage
+    make V=1 -j $NIX_BUILD_CORES isoimage
   '';
 
   patchPhase = ''
@@ -48,7 +46,7 @@ pkgs.stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out
     ls -la
-    cp nautilus.bin $out/
+    cp nautilus.* $out/
     # cp nautilus.iso $out/
   '';
 }
