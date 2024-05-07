e52f8250389a60f6629d6d2fb88f34231462f80b
Kevin Hayes <KevinHayes2026@u.northwestern.edu>
2024-05-07 16:29:50 -0500

    Update sincos Shell Command
    Made the sincos command take an argument (still has to be an integer
    because sscanf doesn't seem to work with %f). Also made the actual
    command "sincos" like the help message, not "sc".
    
    There are two copies of this command still becasue there are two copies
    of all the math function (one for libm and one for NAS) which is a
    bigger issue which I'll get around to addressing later.

--- src/compat/libmcompat/math/sincos/sincos_test.c
+++ src/compat/libmcompat/math/sincos/sincos_test.c
@@ -2,20 +2,32 @@
 #include <nautilus/shell.h>
 #include "nas_sincos.h"
 
+int printf(const char*, ...);
 static int sincos(char *__buf, void* __priv);
 
+double sin(double);
+double cos(double);
+
 static struct shell_cmd_impl nas_sincos_impl = {
-    .cmd      = "sc",
-    .help_str = "sincos test",
+    .cmd      = "sincos",
+    .help_str = "sincos [integer]",
     .handler  = sincos,
 };
 nk_register_shell_cmd(nas_sincos_impl);
 /*--------------------------------------------------------------------
       program BT
 c-------------------------------------------------------------------*/
-static int sincos(char *__buf, void* __priv) {
+static int sincos(char *buf, void* __priv) 
+{
+  int num;
 
-  nk_vc_printf("sin : %lf, cos : %lf \n", sin(0.567),cos(9.087));
+  if(sscanf(buf, "sincos %d", &num) != 1) {
+      printf("Unrecognized arguments: usage: sincos [integer]\n");
+      return 0;
+  }
+
+  double d = (double)num;
+  printf("sin(%8.8lf) = %8.8lf, cos(%8.8lf) = %8.8lf\n",d,sin(d),d,cos(d));
   return 0;
 }
 
